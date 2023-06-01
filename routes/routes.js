794ea83544470f70e279ff63c797c072cd4daab1
alonso0705 <122815424+alonso0705@users.noreply.github.com>
2023-06-02 01:57:16 +0700

    auth fixed

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { login } from "../controllers/authController.js"
 import { init, getTTS } from "../controllers/fakeYouController.js"
 import { makeMessage } from "../controllers/chatGptController.js"
 
 const router = express.Router();
 
+router.post("/login", login);
 router.post("/tts_init", init);
 router.post("/get_tts", getTTS);
 router.post("/make_message", makeMessage)
