394f9b8810783d963df940a5eb763bec3e49af31
alonso0705 <122815424+alonso0705@users.noreply.github.com>
2023-06-03 01:42:46 +0700

    autoplay fixed

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.post("/login", login);
 router.post("/tts_init", init);
 router.post("/get_tts", getTTS);
-router.post("/make_message", makeMessage)
+router.post("/make_message", makeMessage);
 
 export default router;
