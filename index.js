4ebb4baa25d1d1cd08eda9ce906a71d5576676c0
alonso0705 <122815424+alonso0705@users.noreply.github.com>
2023-06-10 17:22:47 +0700

    deploy-3

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.use((cors()))
 app.use(express.static(path.join(__dirname, 'public')))
 app.enable('trust proxy')
 
-// dbConnect();
-// seedPrompts()
+dbConnect();
+seedPrompts()
 
 // User Routes
 app.use('/', route)
